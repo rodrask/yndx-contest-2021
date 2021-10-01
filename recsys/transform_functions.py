c35fe6d5beac0f2e5f8660f7e76d6902ce7f7af9
Pavel Okunev <p.okunev@corp.mail.ru>
2021-10-01 18:56:05 +0300

    add transform functions

--- recsys/transform_functions.py
+++ recsys/transform_functions.py
@@ -1,7 +1,15 @@
 from sklearn.preprocessing import LabelEncoder
+from scipy import sparse
+import numpy as np
 
 def index_items(items):
     encoder = LabelEncoder()
     encoder.fit(items)
     return encoder
-     
+
+def reviews_to_matrix(reviews, users_encoder, orgs_encoder):
+    users_idx = users_encoder.transform(reviews['user_id'])
+    org_idx = orgs_encoder.transform(reviews['org_id'])
+    data = np.ones_like(users_idx)
+    return sparse.coo_matrix((data, (users_idx, org_idx)))
+
